b92d95b69767ae223683e1a02075c37596ef0a40
Ryosuke Yabuki <ryabuki@launchableinc.com>
2022-06-16 10:10:48 +0900

    use Set type instead of Map

--- src/main/java/com/launchableinc/testng/TestSelector.java
+++ src/main/java/com/launchableinc/testng/TestSelector.java
@@ -3,9 +3,9 @@ package com.launchableinc.testng;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Logger;
 import org.kohsuke.MetaInfServices;
 import org.testng.IMethodInstance;
@@ -30,11 +30,11 @@ public class TestSelector implements IMethodInterceptor {
 				System.getenv(LAUNCHABLE_SUBSET_FILE) == null ? LAUNCHABLE_DEFAULT_SUBSET_FILE_PATH
 						: System.getenv(LAUNCHABLE_SUBSET_FILE);
 
-		Map<String, String> subsetList = new HashMap<>();
+		Set<String> subsetList = new HashSet<>();
 		try (Scanner scanner = new Scanner(new FileReader(subsetFilePath))) {
 			while (scanner.hasNext()) {
 				String l = scanner.nextLine();
-				subsetList.put(l, l);
+				subsetList.add(l);
 			}
 		} catch (FileNotFoundException e) {
 			LOGGER.warning(String.format(
@@ -45,7 +45,7 @@ public class TestSelector implements IMethodInterceptor {
 		Iterator<IMethodInstance> itr = methods.iterator();
 		while (itr.hasNext()) {
 			IMethodInstance m = itr.next();
-			if (subsetList.get(m.getMethod().getTestClass().getName()) == null) {
+			if (subsetList.contains(m.getMethod().getTestClass().getName())) {
 				itr.remove();
 			}
 		}
